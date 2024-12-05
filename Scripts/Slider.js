bfabbc1d6b3b05b5c9ff4eda817701f6b650c8b3
Svetlana Bedareva <54273014+SvetlanaBedareva@users.noreply.github.com>
2024-12-04 21:01:40 -0500

    updating styles and adding Contact and About us pages

--- Scripts/Slider.js
+++ Scripts/Slider.js
@@ -1,3 +1,10 @@
+/*
+@Title - Final Assignment
+@Authors - Monkhouse, Braidan P, Svetlana Bedareva, Jamie Bascin
+@Updated - November 12, 2024
+*/
+
+
 const sliderWrapper = document.querySelector('.slider-wrapper');
   const slides = document.querySelectorAll('.slide');
   const leftButton = document.querySelector('.left-button');
@@ -5,11 +12,13 @@ const sliderWrapper = document.querySelector('.slider-wrapper');
 
   let currentIndex = 0;
 
+// Initialize and update slider navigation buttons based on the current slide index
   const updateButtons = () => {
     leftButton.classList.toggle('disabled', currentIndex === 0);
     rightButton.classList.toggle('disabled', currentIndex === slides.length - 1);
   };
 
+  // Scroll to the specified slide smoothly and update the button states
   const scrollToSlide = (index) => {
     console.log('index', index)
     const targetSlide = slides[index];
